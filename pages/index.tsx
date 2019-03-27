27d1c613e19076933be7ca683016ba32258d07fe
Alexander Wilkinson <alexwilkinson@gmail.com>
2019-03-27 23:15:57 +0800

    updated index to start to look like my existing page to try and recreate and update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,25 @@
-import * as React from "react";
-import Link from "next/link";
-import { connect } from "react-redux";
-import Layout from "../components/_Layout/Layout";
-import { Dispatch } from "redux";
-import graphQLQuery, {
-  graphQLFetch
-} from "../components/graphQL/GraphQLWrapper";
-import { BLOG_LIST_QUERY } from "../graphQL/queries/blogQuery";
-import * as actions from "../store/blog/actions";
-import { Blog } from "../server/entity/MyBlog";
-import { UserState } from "../store/user/reducers";
-import Header from "../components/_Layout/Header";
+import * as React from 'react';
+import Link from 'next/link';
+import {connect} from 'react-redux';
+import Layout from '../components/_Layout/Layout';
+import {Dispatch} from 'redux';
+import graphQLQuery, {graphQLFetch} from '../components/graphQL/GraphQLWrapper';
+import {BLOG_LIST_QUERY} from '../graphQL/queries/blogQuery';
+import * as actions from '../store/blog/actions';
+import {Blog} from '../server/entity/MyBlog';
+import {UserState} from '../store/user/reducers';
+import Header from '../components/_Layout/Header';
+
+const Hl = ({children}) => (
+  <React.Fragment>
+    <span>{children}</span>
+    <style jsx>{`
+      span {
+        color: #4196ea;
+      }
+    `}</style>
+  </React.Fragment>
+);
 
 interface Props {
   getBlogList: (token?) => void;
@@ -23,28 +32,37 @@ interface State {}
 
 export class IndexPage extends React.Component<Props, State> {
   componentDidMount() {
-    console.log("component mounted");
-    const { token } = this.props.userSession;
+    console.log('component mounted');
+    const {token} = this.props.userSession;
     this.props.getBlogList(token);
   }
 
   public render() {
     return (
       <Layout displayImg showFooter={false}>
-        <h1>Index</h1>
+        <h1>
+          Hi! I'm <Hl>Alex</Hl> a full stack developer with some design flair
+        </h1>
+        <h3>
+          Located in <Hl>Singapore</Hl> I have experience with everything from
+          Android development to SCSS. I started in a more backend role with
+          Java but now work full stack with Asp.Net Core & Angular. I'm
+          currently really exploring the JavaScript everywhere paradigm
+          exploring NodeJs, Electron & VueJS.
+        </h3>
       </Layout>
     );
   }
 }
 
-const mapStateToProps = ({ blog, user }: any) => ({
+const mapStateToProps = ({blog, user}: any) => ({
   userSession: user,
   blogList: blog.blogList,
-  fetching: blog.fetching
+  fetching: blog.fetching,
 });
 
 const mapDispatchToProps = (dispatch: any): any => ({
-  getBlogList: (token): any => dispatch(actions.fetchBlogList(token))
+  getBlogList: (token): any => dispatch(actions.fetchBlogList(token)),
 });
 
 export default connect(
