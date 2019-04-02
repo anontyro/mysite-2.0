a84c417eb745d1574d2c27025613d157374453ba
Alexander Wilkinson <alexwilkinson@gmail.com>
2019-04-02 22:25:45 +0800

    added in string util tests

--- utils/stringUtil.test.ts
+++ utils/stringUtil.test.ts
@@ -1,11 +1,19 @@
-import anyTest, { TestInterface } from "ava";
-import { createBlurb } from "./stringUtil";
+import anyTest, {TestInterface} from 'ava';
+import {createBlurb} from './stringUtil';
 
 const test = anyTest as TestInterface<{}>;
 
-test("createBlurb will return a string", t => {
-  const text = "this is some text";
+test('createBlurb will return a string that is the same if below the threashold', t => {
+  const text = 'this is some text';
   const output = createBlurb(text);
 
-  t.truthy(output);
+  t.deepEqual(text, output);
+});
+
+test('createBlurb will shorten a long string and add ellipsis', t => {
+  const text =
+    'this is more text this is more text this is more text this is more text this is more text this is more text this is more text this is more text this is more text this is more text';
+  const output = createBlurb(text);
+  const expected = `${text.substr(0, 50)}...`;
+  t.deepEqual(output, expected);
 });
