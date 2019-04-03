bec31b9e2bcfacfb75bcfe420ea0da0a239d0e13
Alexander Wilkinson <alexwilkinson@gmail.com>
2019-04-03 21:50:56 +0800

    added in the basic resume and load the data

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {connect} from 'react-redux';
 import Layout from '../components/_Layout/Layout';
+import * as actions from '../store/resume/actions';
+import {ResumeState} from '../store/resume/reducers';
+import IsLoading from '../components/util/IsLoading';
 
-interface Props {}
+interface Props {
+  getResume: () => void;
+  resume: ResumeState;
+}
 
-const ResumePage = ({}: Props) => {
+const ResumePage = ({getResume, resume}: Props) => {
+  useEffect(() => {
+    getResume();
+  }, []);
+
   return (
     <Layout title="Resume">
-      <React.Fragment>
-        <h1>Resume page</h1>
-      </React.Fragment>
+      <IsLoading isLoading={resume.isLoading}>
+        <React.Fragment>
+          <h1>Resume page</h1>
+        </React.Fragment>
+      </IsLoading>
     </Layout>
   );
 };
 
-const mapStateToProps = ({}) => ({});
+const mapStateToProps = ({resume}: any) => ({
+  resume: resume,
+});
 
-const mapDispatchToProps = (dispatch: any) => ({});
+const mapDispatchToProps = (dispatch: any) => ({
+  getResume: (): any => dispatch(actions.fetchResume()),
+});
 
 export default connect(
   mapStateToProps,
