35024248fdd06a04c63912466d031e30f563f510
Alexander Wilkinson <alexwilkinson@gmail.com>
2019-04-16 23:05:08 +0800

    updated consts

--- data/typo.ts
+++ data/typo.ts
@@ -3,6 +3,7 @@ export const brandFont = `'Pacifico',cursive`;
 export const secondaryBlueColor = '#4196ea';
 
 export const brandDesign = `
+  text-decoration: none;
   font-size: 30px;
   line-height: 31px;
   font-family: ${brandFont};
