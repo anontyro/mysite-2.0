01398048a94b220dea70c2a849ee7d07e659e6d1
Alexander Wilkinson <alexwilkinson@gmail.com>
2019-04-16 23:04:41 +0800

    added in admin section which will act as the cms section for authorised users

---
+++ pages/admin.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Link from 'next/link';
+import Layout from '../components/_Layout/Layout';
+
+const AdminPage: React.FunctionComponent = () => (
+  <Layout title="Admin">
+    <h1>Admin</h1>
+  </Layout>
+);
+
+export default AdminPage;
