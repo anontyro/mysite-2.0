5e58131ebe948d6a2130aab9fbf954e42e8bb0e0
Alexander Wilkinson <alexwilkinson@gmail.com>
2019-04-23 22:31:29 +0800

    updated actions to redirect correctly and added in logout in admin

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,11 +1,42 @@
 import * as React from 'react';
-import Link from 'next/link';
+import {connect} from 'react-redux';
 import Layout from '../components/_Layout/Layout';
+import * as actions from '../store/user/actions';
+import IsLoggedIn from '../components/util/IsLoggedIn';
+import {UserLogin, UserState} from '../store/user/reducers';
+import LogoutPage from '../components/auth/Logout';
 
-const AdminPage: React.FunctionComponent = () => (
-  <Layout title="Admin">
-    <h1>Admin</h1>
-  </Layout>
-);
+interface Props {
+  userSession: UserState;
+  logout: () => void;
+}
 
-export default AdminPage;
+const AdminPage = ({logout, userSession}: Props) => {
+  const onLogout = (event: any) => {
+    event.preventDefault();
+    logout();
+  };
+
+  return (
+    <Layout title="Admin">
+      <h1>Admin</h1>
+      <IsLoggedIn>
+        <LogoutPage onLogout={onLogout} userSession={userSession} />
+      </IsLoggedIn>
+    </Layout>
+  );
+};
+
+const mapStateToProps = ({user}: any) => ({
+  userSession: user,
+});
+
+const mapDispatchToProps = (dispatch: any): any => ({
+  loginUser: (userLogin: UserLogin) => dispatch(actions.loginUser(userLogin)),
+  logout: () => dispatch(actions.logoutUser()),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AdminPage);
