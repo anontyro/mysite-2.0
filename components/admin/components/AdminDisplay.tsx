73d34e245e47f0912602a5953483ddf7c84699bb
Alexander Wilkinson <alexwilkinson@gmail.com>
2019-05-22 21:40:35 +0800

    [Admin] updated AdminDisplay with new edit component and some small clean up

--- components/admin/components/AdminDisplay.tsx
+++ components/admin/components/AdminDisplay.tsx
@@ -3,6 +3,7 @@ import {ADMIN_SECTIONS} from '../../../data/consts';
 import AdminBlog from '../Blog';
 import AdminAccount from '../User';
 import AdminHome from '../Home';
+import BlogEdit from '../Blog/BlogEdit';
 
 interface AdminProps {
   section?: string | string[];
@@ -12,8 +13,9 @@ const AdminDisplay = ({section = 'home'}: AdminProps) => {
   switch (section) {
     case ADMIN_SECTIONS.BLOG_HOME:
     case ADMIN_SECTIONS.BLOG_ADD:
-    case ADMIN_SECTIONS.BLOG_EDIT:
       return <AdminBlog />;
+    case ADMIN_SECTIONS.BLOG_EDIT:
+      return <BlogEdit />;
     case ADMIN_SECTIONS.USER:
       return <AdminAccount />;
     default:
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {useState, useEffect} from 'react';
+import {useState} from 'react';
 import {connect} from 'react-redux';
 import {UserLogin, UserState} from '../store/user/reducers';
 import * as actions from '../store/user/actions';
