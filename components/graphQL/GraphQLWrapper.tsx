18efa0ed3e82df79ff5b71b7168fccc0fd3100fa
Alexander Wilkinson <alexwilkinson@gmail.com>
2019-10-06 00:51:08 +0800

    added in k8 configs updated build action and created proper handling of GraphQL instances from prod to nightly

--- components/graphQL/GraphQLWrapper.tsx
+++ components/graphQL/GraphQLWrapper.tsx
@@ -11,7 +11,16 @@ export const GRAPHQL_URL_DEV = 'https://nightly.alexwilkinson.co/graphql';
 export const GRAPHQL_URL_PROD = 'https://alexwilkinson.co/graphql';
 export const GRAPHQL_BGG_EX = 'https://bgg-api.alexwilkinson.co/graphql';
 
-const uri = GRAPHQL_URL_DEV;
+const getGraphQlEnvUri = (): string => {
+  const isDev = process.env.NODE_ENV !== 'production';
+
+  if (isDev) {
+    return GRAPHQL_URL_DEV;
+  }
+  return GRAPHQL_URL_PROD;
+};
+
+const uri = getGraphQlEnvUri();
 const link = new HttpLink({uri, fetch});
 
 const graphQLQuery = ({
